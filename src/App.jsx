ded0bdcf3794ddee9839580ebac7e4774a2014ce
Benk513 <79744201+Benk513@users.noreply.github.com>
2024-10-26 22:18:52 +0100

    feat:add Form from react router it handle all the actions w/0 creating state ,
    the request parameter is get when the submission is triggered,
    
    we need to convert the formData to an object for easy reading

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ import Home from './ui/Home'
 import Menu, {loader as menuLoader} from './features/menu/Menu'
 import Order, {loader as orderLoader} from './features/order/Order'
 import Cart from './features/cart/Cart'
-import CreateOrder from './features/order/CreateOrder'
+import CreateOrder ,{action as createOrderAction} from './features/order/CreateOrder'
  import AppLayout from './ui/AppLayout';
 import Error from './ui/Error'
 
@@ -28,7 +28,8 @@ import Error from './ui/Error'
     },
     {
       path: "/order/new",
-      element: <CreateOrder/>
+      element: <CreateOrder/>,
+      action:createOrderAction
     },
     {
       path: "/order/:orderId",
