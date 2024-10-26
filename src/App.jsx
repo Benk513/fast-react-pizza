a36041b21d9e0eb6bdd5fa13e50a8ca2bbee3dcd
Benk513 <79744201+Benk513@users.noreply.github.com>
2024-10-26 11:33:14 +0100

    feat : implement loader from router with useNavigation()

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,12 @@
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import './App.css'
 import Home from './ui/Home'
-import Menu from './features/menu/Menu'
+import Menu, {loader as menuLoader} from './features/menu/Menu'
 import Cart from './features/cart/Cart'
 import CreateOrder from './features/order/CreateOrder'
 import Order from './features/order/Order'
 import AppLayout from './ui/AppLayout';
+
    const router = createBrowserRouter([  
                                               
     {element:<AppLayout/>,
@@ -16,8 +17,8 @@ import AppLayout from './ui/AppLayout';
     },
     {
       path: "/menu",
-      element: <Menu/>
-    },
+      element: <Menu/>,
+    loader: menuLoader   },
     {
       path: "/cart",
       element: <Cart/>
--- src/ui/AppLayout.jsx
+++ src/ui/AppLayout.jsx
@@ -1,11 +1,16 @@
-import { Outlet } from "react-router-dom"
+import { Outlet , useNavigation } from "react-router-dom"
 import CartOverview from "../features/cart/CartOverview"
 import Header from "./Header"
-
+import Loading from "./../ui/Loading"
  
 const AppLayout = () => {
+
+  const navigation  = useNavigation()
+  const isLoading = navigation.state ==='loading'
+  
   return (
-    <div>
+    <div className="layout">
+    {isLoading &&<Loading/>}
     <Header/>
     <main>
        <Outlet/>
