5f7c9f50b9708fbae13511d4e591169751414084
Benk513 <79744201+Benk513@users.noreply.github.com>
2024-11-16 17:40:28 +0100

    style

--- src/features/menu/Menu.jsx
+++ src/features/menu/Menu.jsx
@@ -6,21 +6,15 @@ function Menu() {
   const menu = useLoaderData();
   console.log(menu)
   return(
-    <ul>
+    <ul className="divide-y divide-stone-200 px-2">
       { menu.map(pizza => <MenuItem key={pizza.id} pizza={pizza}/>)}
-
     </ul>
   );
 }
 
-
-
 export async function loader(){
-
   const menu = await getMenu();
-
     return menu
-
 }
 
 export default Menu;
