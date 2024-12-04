4b1069f6b0c223cda7d2146dfa741a935e99db12
Benk513 <79744201+Benk513@users.noreply.github.com>
2024-12-04 17:29:00 +0100

    feat:Oreder Details using useFetcher to load data from other route w/0 navigation

--- src/features/order/OrderItem.jsx
+++ src/features/order/OrderItem.jsx
@@ -1,14 +1,19 @@
+import { formatCurrency } from "../../utils/helpers";
+
 function OrderItem({ item, isLoadingIngredients, ingredients }) {
   const { quantity, name, totalPrice } = item;
 
   return (
-    <li>
-      <div>
-        <p>
+    <li className="space-y-1 py-3">
+      <div  className="flex items-center justify-between gap-4 text-sm">
+        <p className="font-bold">
           <span>{quantity}&times;</span> {name}
         </p>
-        <p>{formatCurrency(totalPrice)}</p>
+        <p  className="font-bold">{formatCurrency(totalPrice)}</p>
       </div>
+      <p className="text-sm capitalize italic text-stone-500">
+        {isLoadingIngredients ? 'Loading...' : ingredients.join(', ')}
+      </p>
     </li>
   );
 }
